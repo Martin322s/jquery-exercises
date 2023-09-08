d7ed44daed92515bc1b0b740a80290132a44a36c
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-09-08 20:12:55 +0300

    exercise 2

--- 02. Search In List/app.js
+++ 02. Search In List/app.js
@@ -1,3 +1,10 @@
 function search() {
-    // TODO
+    $(`#towns li`).css('font-weight', 'normal');
+
+    let inputValue = $('#searchText').val();
+
+    let towns = $(`#towns li:contains(${inputValue})`).css('font-weight', 'bold');
+    let count = towns.toArray().length;
+
+    $('#result').text(`${count} matches found.`);
 }
