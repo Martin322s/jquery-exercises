d21f9fc75184bea15c43c4c2f21a46d54e5e6f99
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-09-11 23:19:15 +0300

    get input data

--- 11. Ski Lift/app.js
+++ 11. Ski Lift/app.js
@@ -1,9 +1,23 @@
 window.addEventListener('load', solve);
 
 function solve() {
-   
+    let $firstName = $('#first-name');
+    let $lastName = $('#last-name');
+    let $peopleCount = $('#people-count');
+    let $dateFrom = $('#from-date');
+    let $daysCount = $('#days-count');
+    let $nextBtn = $('#next-btn');
+    let ticketList = $('ticket-info-list');
+    let $confirmList = $('confirm-ticket');
+
+    $nextBtn.on('click', (e) => {
+        e.preventDefault();
+
+        if ($firstName.val() === '' || $lastName.val() === '' ||
+            $peopleCount.val() === '' || $dateFrom.val() === '' || $daysCount.val() === '') {
+            alert('All fields must be filled required!');
+        } else {
+            
+        }
+    });
 }
-
-
-    
-    
