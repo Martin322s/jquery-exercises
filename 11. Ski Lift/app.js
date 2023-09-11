e0010e8d65c73e9f19a0072942a925ea67d7b833
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-09-11 23:31:45 +0300

    add input info to the tickets review

--- 11. Ski Lift/app.js
+++ 11. Ski Lift/app.js
@@ -7,8 +7,8 @@ function solve() {
     let $dateFrom = $('#from-date');
     let $daysCount = $('#days-count');
     let $nextBtn = $('#next-btn');
-    let ticketList = $('ticket-info-list');
-    let $confirmList = $('confirm-ticket');
+    let $ticketList = $('.ticket-info-list');
+    let $confirmList = $('.confirm-ticket');
 
     $nextBtn.on('click', (e) => {
         e.preventDefault();
@@ -17,7 +17,28 @@ function solve() {
             $peopleCount.val() === '' || $dateFrom.val() === '' || $daysCount.val() === '') {
             alert('All fields must be filled required!');
         } else {
-            
+            let $liElement = $('<li>').attr('class', 'ticket');
+            let $articleInfo = $('<article>');
+            let $pName = $('<h3>').text(`Name: ${$firstName.val()} ${$lastName.val()}`);
+            let $pDate = $('<p>').text(`From date: ${$dateFrom.val()}`);
+            let $pDays = $('<p>').text(`For ${$peopleCount.val()} days`);
+            let $pPeople = $('<p>').text(`For ${$peopleCount.val()} people`);
+            let $buttonEdit = $('<button>').attr('class', 'edit-btn').text('Edit');
+            let $buttonContinue = $('<button>').attr('class', 'continue-btn').text('Continue');
+
+            $articleInfo.append($pName);
+            $articleInfo.append($pDate);
+            $articleInfo.append($pDays);
+            $articleInfo.append($pPeople);
+            $articleInfo.append($buttonEdit);
+            $articleInfo.append($buttonContinue);
+
+            $liElement.append($articleInfo);
+            $ticketList.append($liElement);
+
+            $nextBtn.attr('disabled', 'true');
+            $('input').val('');
+            $('select').val('');
         }
     });
 }
