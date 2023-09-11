8c0fffecc2444e13907249b8ce8ad583768b3f22
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-09-11 19:57:39 +0300

    get all input fields

--- 10. Fitness Journey/app.js
+++ 10. Fitness Journey/app.js
@@ -1,9 +1,17 @@
 window.addEventListener('load', solve);
 
 function solve() {
-//TODO...
+    let $name = $('#name');
+    let $email = $('#email');
+    let $contactNumber = $('#contact-number');
+    let $classType = $('#class-type');
+    let $classTime = $('#class-time');
+    let $nextBtn = $('#next-btn');
+    let $classInfo = $('.class-info');
+
+    $nextBtn.on('click', (e) => {
+        e.preventDefault();
+
+        
+    });
 }
-
-
-
-
