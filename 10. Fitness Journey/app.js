b20cbfda4a446d06373372f43b5e3a48e18ba386
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-09-11 20:20:29 +0300

    create new elements on click ev

--- 10. Fitness Journey/app.js
+++ 10. Fitness Journey/app.js
@@ -7,7 +7,7 @@ function solve() {
     let $classType = $('#class-type');
     let $classTime = $('#class-time');
     let $nextBtn = $('#next-btn');
-    let $classInfo = $('.class-info');
+    let $infoList = $('.class-info');
 
     $nextBtn.on('click', (e) => {
         e.preventDefault();
@@ -16,7 +16,30 @@ function solve() {
             $contactNumber.val() === '' || $classType.val() === '' || $classTime.val() === '') {
             alert('All fields must be filled required!');
         } else {
+            let $liElement = $('<li>').attr('class', 'info-item');
+            let $articleInfo = $('<article>').attr('class', 'personal-info');
+            let $pName = $('<p>').text($name.val());
+            let $pEmail = $('<p>').text($email.val());
+            let $pPhone = $('<p>').text($contactNumber.val());
+            let $pType = $('<p>').text($classType.val());
+            let $pTime = $('<p>').text($classTime.val());
+            let $buttonEdit = $('<button>').attr('class', 'edit-btn').text('Edit');
+            let $buttonContinue = $('<button>').attr('class', 'continue-btn').text('Continue');
 
+            $articleInfo.append($pName);
+            $articleInfo.append($pEmail);
+            $articleInfo.append($pPhone);
+            $articleInfo.append($pType);
+            $articleInfo.append($pTime);
+            $articleInfo.append($buttonEdit);
+            $articleInfo.append($buttonContinue);
+
+            $liElement.append($articleInfo);
+            $infoList.append($liElement);
+
+            $nextBtn.attr('disabled', 'true');
+            $('input').val('');
+            $('select').val('')
         }
     });
 }
