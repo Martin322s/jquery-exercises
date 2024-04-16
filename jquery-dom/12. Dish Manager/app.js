6726ff36f50491951ab103171cfa5b0d701c02c8
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-04-16 10:18:39 +0300

    validate user input

--- jquery-dom/12. Dish Manager/app.js
+++ jquery-dom/12. Dish Manager/app.js
@@ -7,4 +7,27 @@ function solve() {
     let genderSelectElement = $('#genderSelect');
     let dishInputElement = $('#task');
     let submitButton = $('#form-btn');
+    let progressListElement = $('#in-progress');
+    let progressCountElement = $('progress-count');
+    let userInputValues = {};
+
+    submitButton.on('click', (e) => {
+        e.preventDefault();
+
+        if (
+            firstNameInputElement.val() === '' ||
+            lastNameInputElement.val() ||
+            ageInputElement === '',
+            genderSelectElement.val() === '' ||
+            dishInputElement.val() === ''
+        ) {
+            alert('All fields are required!');
+        } else {
+            userInputValues.firstName = firstNameInputElement.val();
+            userInputValues.lastName = lastNameInputElement.val();
+            userInputValues.age = ageInputElement.val();
+            userInputValues.gender = genderSelectElement.val();
+            userInputValues.dishInfo = dishInputElement.val();
+        }
+    });
 }
